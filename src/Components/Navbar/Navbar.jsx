d63c53dcf7f0ce5fb99faa220634e6de58ece360
unknown <nairmitha2000@gmail.com>
2024-02-05 02:31:18 +0530

    create the Navigation Bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,82 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { IoIosCloseCircle } from "react-icons/io";
+import { TbGridDots } from "react-icons/tb";
+import {useState} from 'react';
+
+
+
 
 const Navbar = () => {
-  return (
-    
+
+    const [active, setActive] = useState('navBar');
+
+    //Function to toggle navBar
+    const showNav = () =>{
+        setActive('navBar activeNavbar');
+    }
+
+    //Function to remove navBar
+    const removeNavbar = () =>{
+        setActive('navBar');
+    }
+
+  return ( 
     <section className="navBarSection">
         <header className="header flex">
+
             <div className="logoDiv">
                 <a href="#" className="logo" >
-                    <h1>NX Travel</h1>
+                    <h1><MdOutlineTravelExplore className="icon"/>NX Travel</h1>
                 </a>
             </div>
+
+            <div className={active}>
+                <ul className="navList flex ">
+                    <li className="navItem">
+                        <a href="#" className="navLink">Home</a>
+                    </li>
+
+                    <li className="navItem">
+                        <a href="#" className="navLink">Packages</a>
+                    </li>
+
+                    <li className="navItem">
+                        <a href="#" className="navLink">Shop</a>
+                    </li>
+
+                    <li className="navItem">
+                        <a href="#" className="navLink">About</a>
+                    </li>
+
+                    <li className="navItem">
+                        <a href="#" className="navLink">Pages</a>
+                    </li>
+
+                    <li className="navItem">
+                        <a href="#" className="navLink">News</a>
+                    </li>
+
+                    <li className="navItem">
+                        <a href="#" className="navLink">Contact</a>
+                    </li>
+
+                    <button className="btn">
+                        <a href="#">BOOK NOW</a>
+                    </button>
+
+                </ul>
+                <div onClick={removeNavbar} className="closeNavbar">
+                    <IoIosCloseCircle className="icon"/>
+                </div>
+
+            </div>
+
+            <div onClick={showNav} className="toggleNavbar">
+                <TbGridDots className="icon"/>
+            </div>
+
         </header>
 
     </section>
